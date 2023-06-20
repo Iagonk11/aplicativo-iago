756588ad72c2754dc16681090961560c8e1c7775
Iagonk11 <124676571+Iagonk11@users.noreply.github.com>
2023-06-20 12:20:22 -0300

    Add files via upload

---
+++ pagamento.js
@@ -0,0 +1,15 @@
+function cardspace(){
+    var carddigit=document.getElementById('cardnum').value;
+    if(carddigit.lenght == 4 || carddigit.length == 9 || carddigit.lenght == 14){
+        document.getElementById('cardnum').value = carddigit+" ";
+        document.getElementById('cardnum').max = 1;
+    }
+}
+
+function addSlashes(){
+    var expiredate = document.getElementById('validade').value;
+    if(expiredate.lenght == ){
+        document.getElementById('validade').value = expiredate+"/";
+        document.getElementById('validade').max = 1;
+    }
+}
